89552f9f306cced804025cd915d2e8382618c3bb
Electricsstrom0 <burhanudinmalik55@gmail.com>
2025-07-09 19:27:51 +0700

    Rename temp-Users to Users

--- src/Infrastructures/container/program_main/hol/users/holUsers.js
+++ src/Infrastructures/container/program_main/hol/users/holUsers.js
@@ -2,7 +2,7 @@ const pool = require('../../../../database/mysql/pool');
 
 // const MasterHOLAreaRepository = require('../../../../../../');
 const HOLUsersRepository = require('../../../../../Domains/program_main/hol/Users/HOLUsersRepository');
-const HOLUsersRepositoryMySQL = require('../../../../repository/program_main/hol/temp-Users/HOLUsersRepositoryMySQL');
+const HOLUsersRepositoryMySQL = require('../../../../repository/program_main/hol/Users/HOLUsersRepositoryMySQL');
 const MasterBatchRepository = require('../../../../../Domains/batch/MasterBatchRepository');
 const MasterProgramRoleRepository = require('../../../../../Domains/program/MasterProgramRoleRepository');
 const OfferedProgramRepository = require('../../../../../Domains/program/OfferedProgramRepository');
