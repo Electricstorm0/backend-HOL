0cad78978c9037fe2a0d109e16d50222f4c47d19
Electricsstrom0 <burhanudinmalik55@gmail.com>
2025-07-10 13:56:37 +0700

    Rename temp-Articles to articles

--- src/Infrastructures/container/program_main/hol/Articles/hol_articles.js
+++ src/Infrastructures/container/program_main/hol/Articles/hol_articles.js
@@ -1,8 +1,8 @@
 // REPOSITORY
 const HOLUsersArticlesRepository = require('../../../../../Domains/program_main/hol/articles/HOLUsersArticlesRepository');
 const MasterHOLArticlesRepository = require('../../../../../Domains/program_main/hol/articles/MasterHOLArticlesRepository');
-const HOLUsersArticlesRepositoryMySQL = require('../../../../repository/program_main/hol/temp-Articles/HOLUsersArticlesRepositoryMySQL');
-const MasterHOLArticlesRepositoryMySQL = require('../../../../repository/program_main/hol/temp-Articles/MasterHOLArticlesRepositoryMySQL');
+const HOLUsersArticlesRepositoryMySQL = require('../../../../repository/program_main/hol/articles/HOLUsersArticlesRepositoryMySQL');
+const MasterHOLArticlesRepositoryMySQL = require('../../../../repository/program_main/hol/articles/MasterHOLArticlesRepositoryMySQL');
 // USECASE
 const HOLCreateArticlesUseCase = require('../../../../../Applications/use_case/Program_Main/HOL/Articles/HOLCreateArticlesUseCase');
 const HOLGetAllMyArticleUseCase = require('../../../../../Applications/use_case/Program_Main/HOL/Articles/HOLGetAllMyArticleUseCase');
