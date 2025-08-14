5d58b4feb4268da6ecf4dcc2c11f437d7ae255ec
Electricsstrom0 <burhanudinmalik55@gmail.com>
2025-08-14 16:27:54 +0700

    fix create users

--- src/Applications/use_case/Program_Main/HOL/Users/HOLCreateUsersUseCase.js
+++ src/Applications/use_case/Program_Main/HOL/Users/HOLCreateUsersUseCase.js
@@ -22,66 +22,63 @@ class HOLCreateUsersUseCase {
   }
 
   async execute(payload) {
-    const { fullname, email, password, thirdTierProgramId, batchId } = new HOLAddUsers(payload);
-    const emailForUsername = email.split('@');
-    const username = emailForUsername[0];
-    const { firstName, lastName } = await this.splitName(fullname);
-    const hashPassword = await this._passwordHash.hash(password);
+    try {
+      const { fullname, email, password, thirdTierProgramId, batchId } = new HOLAddUsers(payload);
+      const emailForUsername = email.split('@');
+      const username = emailForUsername[0];
+      const { firstName, lastName } = await this.splitName(fullname);
+      const hashPassword = await this._passwordHash.hash(password);
 
-    let usersId;
-    const verifyHasEmail = await this._usersRepository.verifyAvailableEmail({ email });
-    if (verifyHasEmail) {
-      throw new InvariantError(`${fullname}, email anda ${email} sudah terdaftar!`);
-    } else {
-      const { id } = await this._usersRepository.create({
-        username,
-        email,
-        password: hashPassword,
-      });
+      let usersId;
+      const verifyHasEmail = await this._usersRepository.verifyAvailableEmail({ email });
+      if (verifyHasEmail) {
+        throw new InvariantError(`${fullname}, email anda ${email} sudah terdaftar!`);
+      } else {
+        const { id } = await this._usersRepository.create({
+          username,
+          email,
+          password: hashPassword,
+        });
 
-      await this._usersDetailRepository.create({
-        usersId: id,
-        cardIdNumber: '-',
-        firstName,
-        lastName,
-      });
+        await this._usersDetailRepository.create({
+          usersId: id,
+          cardIdNumber: '-',
+          firstName,
+          lastName,
+        });
 
-      usersId = id;
+        usersId = id;
 
-      await this._offeredProgramRepository.create({
-        usersId,
-        statusAcceptanceProgramId: 4,
-        thirdTierProgramId,
-        roleId: 4,
-        batchId,
-      });
+        await this._offeredProgramRepository.create({
+          usersId,
+          statusAcceptanceProgramId: 4,
+          thirdTierProgramId,
+          roleId: 4,
+          batchId,
+        });
 
-      await this._holUsersRepository.create({
-        usersId,
-        batchId,
-      });
-      await this._usersDomicileRepository.create({
-        usersId,
-        batchId,
-        status: 'CURRENT',
-      });
-      await this._usersUniversitiesRepository.create({
-        usersId,
-        batchId,
-      });
-
-      if (thirdTierProgramId === 1 || thirdTierProgramId === 2 || thirdTierProgramId === 3) {
-        await this._clpUsersRepository.create({
+        await this._holUsersRepository.create({
+          usersId,
+          batchId,
+        });
+        await this._usersDomicileRepository.create({
+          usersId,
+          batchId,
+          status: 'CURRENT',
+        });
+        await this._usersUniversitiesRepository.create({
           usersId,
           batchId,
         });
       }
+      return {
+        usersId,
+        fullname,
+        email,
+      };
+    } catch (error) {
+      console.log(error);
     }
-    return {
-      usersId,
-      fullname,
-      email,
-    };
   }
 }
 
