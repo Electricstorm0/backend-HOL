19bb0c060e535672b362a04dec09ddb4c4c0aec8
Electricsstrom0 <burhanudinmalik55@gmail.com>
2025-08-24 23:58:49 +0700

    fix entities users Recommendation

--- src/Domains/program_main/hol/recommendations/entities/GetUsersRecommendation.js
+++ src/Domains/program_main/hol/recommendations/entities/GetUsersRecommendation.js
@@ -1,6 +1,7 @@
 class getUsersRecommendation {
   constructor(payload) {
-    const { Alumni_Name, Program, Batch, Year, deadline, status } = payload;
+    const { id, Alumni_Name, Program, Batch, Year, deadline, status } = payload;
+    this.recommendationId = id;
     this.alumniName = Alumni_Name;
     this.program = Program;
     this.batch = Batch;
